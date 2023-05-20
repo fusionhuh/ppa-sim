51b0aeb434bb5313e1dc2221cd2ee8a68d85e505
Joshua Adam Karpel <jak1349@ras.ece.northwestern.edu>
2023-05-20 17:16:24 -0500

    Remove outer-level GateSize folder and modify gitignore

--- synthesis/script.tcl
+++ synthesis/script.tcl
@@ -1,13 +1,13 @@
-read_hdl ../verilog/logic/pos_operator.v ../verilog/logic/neg_operator.v ../verilog/logic/carry_operator.v ../verilog/base/ks/ks32.v
+read_hdl ../verilog/logic/pos_operator.v ../verilog/logic/neg_operator.v ../verilog/logic/carry_operator.v ../verilog/base/serial/serial32.v
 set_db library ./NangateOpenCellLibrary_typical.lib
 set_db lef_library ./NangateOpenCellLibrary.lef
 elaborate
-current_design designs/ks32
+current_design designs/serial32
 read_sdc ./hardware_info.sdc
 syn_generic
 syn_map
 syn_opt
-write_hdl > synthesized/base/ks//ks32.v
-report_area > synthesized/base/ks//ks32_area
-report_timing > synthesized/base/ks//ks32_timing
+write_hdl > synthesized/base/serial//serial32.v
+report_area > synthesized/base/serial//serial32_area
+report_timing > synthesized/base/serial//serial32_timing
 
